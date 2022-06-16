5bbaba1cfe569f1b9dabc72be2e49488e3993d5e
beberiche <whdgus269@gmail.com>
2022-06-17 01:52:19 +0900

    220617 BOJ

---
+++ backJoon/src/Q2110_공유기설치/Main.java
@@ -0,0 +1,44 @@
+package Q2110_공유기설치;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int C = sc.nextInt();
+
+		int[] home = new int[N];
+		int max = 0;
+		for (int i = 0; i < N; i++)
+			home[i] = sc.nextInt();
+
+		Arrays.sort(home);
+
+		int st = 1;
+		int ed = home[N - 1] - home[0];
+
+		while (st <= ed) {
+			int mid = (st + ed) / 2;
+			int cnt = 1;
+			int tempSt = home[0];
+			for (int i = 1; i < home.length; i++) {
+				if (home[i] - tempSt >= mid) {
+					cnt++;
+					tempSt = home[i];
+				}
+			}
+			if (cnt < C)
+				ed = mid - 1;
+			else {
+				max = Math.max(max, mid);
+				st = mid + 1;
+			}
+		}
+
+		System.out.println(max);
+
+	}
+}
