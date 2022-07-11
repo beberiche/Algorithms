e8f44239814fd09e33b1b88382a56f1db10ec21e
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-07-11 13:18:44 +0900

    220711 programmers ssafy

--- Programmers/src/네트워크/Solution.java
+++ Programmers/src/네트워크/Solution.java
@@ -1,39 +1,44 @@
 package 네트워크;
 
 import java.util.*;
+
 class Solution {
-    static int[] p;
-    public int solution(int n, int[][] computers) {        
-        p = new int[n];
-        
-        for(int i=0;i<n;i++){
-            p[i]= i;
-        }
-        
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++){
-                if(i==j) continue;
-                if(computers[i][j]==0) continue;
-                
-                int p1 = find(i);
-                int p2 = find(j);
-                if(p1!=p2)  
-                    p[p1]=p2;
-            }
-        }
-        
-        List<Integer> network = new ArrayList<>();
-        for(int i=0; i<n; i++) {
-            int rep= find(i);
-            if(!network.contains(rep)) 
-                network.add(rep);
-        }
-        
-        return network.size();
-    }
-    private static int find(int rep) {
-        if(p[rep]==rep) 
-            return rep;
-        return p[rep] = find(p[rep]);
-    }
+	static int[] p;
+
+	public int solution(int n, int[][] computers) {
+		p = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			p[i] = i;
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i == j)
+					continue;
+				if (computers[i][j] == 0)
+					continue;
+
+				int p1 = find(i);
+				int p2 = find(j);
+				if (p1 != p2)
+					p[p1] = p2;
+			}
+		}
+
+		List<Integer> network = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			int rep = find(i);
+			if (!network.contains(rep))
+				network.add(rep);
+		}
+
+		return network.size();
+	}
+
+	private static int find(int rep) {
+		if (p[rep] == rep)
+			return rep;
+		return p[rep] = find(p[rep]);
+	}
 }
