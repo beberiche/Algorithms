0fa00941b98725c2db82af640b29ee2693a4ef28
beberiche <woaol@naver.com>
2022-12-01 00:01:02 +0900

    221201 BOJ

---
+++ backJoon/src/Q4948_베르트랑_공준/Main.java
@@ -0,0 +1,10 @@
+package Q4948_베르트랑_공준;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("이제됨");
+    }
+}
