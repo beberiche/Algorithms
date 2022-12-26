f142131e74f92c668954e58860c6d3390f53ee66
beberiche <woaol@naver.com>
2022-12-26 23:23:56 +0900

    221226 BOJ

--- backJoon/src/Q18290_NM과_K_1/Main.java
+++ backJoon/src/Q18290_NM과_K_1/Main.java
@@ -37,10 +37,10 @@ public class Main {
             return;
         }
 
-        int is = c + 1 >= M ? r + 1 : r;
-        int js = c + 1 >= M? 0 : c + 1;
-        for (int i = is; i < N; i++) {
-            for (int j = js; j < M; j++) {
+        int tempI = c + 1 >= M ? r + 1 : r;
+        int tempJ = c + 1 >= M? 0 : c + 1;
+        for (int i = tempI; i < N; i++) {
+            for (int j = tempJ; j < M; j++) {
                 int mi = i - 1;
                 int mj = j - 1;
                 if (mi >= 0 && visited[mi][j]) continue;
@@ -50,7 +50,7 @@ public class Main {
                 comb(i, j, cnt + 1, sum + map[i][j]);
                 visited[i][j] = false;
             }
-            js = 0;
+            tempJ = 0;
         }
     }
 }
