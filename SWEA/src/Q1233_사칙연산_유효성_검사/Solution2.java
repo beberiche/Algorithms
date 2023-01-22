14ad11707b41e19e040c932046909cf86839d183
beberiche <woaol@naver.com>
2023-01-23 04:00:10 +0900

    230123 SWEA

--- SWEA/src/Q1233_사칙연산_유효성_검사/Solution2.java
+++ SWEA/src/Q1233_사칙연산_유효성_검사/Solution2.java
@@ -2,25 +2,25 @@ package Q1233_사칙연산_유효성_검사;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
 public class Solution2 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int t = 0, T = 10;
+        StringBuilder sb = new StringBuilder(), sb2;
+        int t = 0, T = 10,N;
+        boolean check;
+        String[] strArr;
+        String op = "+/*-";
         while (++t <= T) {
             sb.append("#").append(t).append(" ");
-            int N = Integer.parseInt(br.readLine());
-            boolean check = true;
+            N = Integer.parseInt(br.readLine());
+            check = true;
             for(int i=0; i<N; i++) {
-                String[] strArr = br.readLine().split(" ");
-                if(strArr.length >= 4 && strArr[1].matches("[0-9]+")) check = false;
+                strArr = br.readLine().split(" ");
+                if(strArr.length >= 4 && !op.contains(strArr[1])) check = false;
             }
-
-            if(!check) sb.append(0);
-            else sb.append(1);
+            sb2 = !check ? sb.append(0) : sb.append(1);
             sb.append("\n");
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
