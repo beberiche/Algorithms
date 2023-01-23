4f45695f1575983c2ac142ff9d360f532fbc5c13
beberiche <woaol@naver.com>
2023-01-24 03:38:44 +0900

    230124 SWEA

---
+++ SWEA/src/Q1232_사칙연산/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node {
+    int num;
+    string op;
+    struct Node *left;
+    struct Node *right;
+};
+int N, T;
+int idx, l, r;
+string op;
+Node nodeArr[1004];
+
+int postOrder(Node node) {
+    if (!node.left || !node.right)
+        return node.num;
+
+    int left = postOrder(*node.left);
+    int right = postOrder(*node.right);
+    if (node.op == "-")
+        return left - right;
+    else if (node.op == "+")
+        return left + right;
+    else if (node.op == "*")
+        return left * right;
+    else
+        return left / right;
+}
+
+int main() {
+    T = 10;
+    for (int t = 1; t <= T; t++) {
+        cout << "#" << t << " ";
+        cin >> N;
+        memset(nodeArr, 0, sizeof(nodeArr));
+        for (int i = 0; i < N; i++) {
+            cin >> idx >> op;
+            if (cin.get() != '\n') {
+                cin >> l >> r;
+                nodeArr[idx].op = op;
+                nodeArr[idx].left = &nodeArr[l];
+                nodeArr[idx].right = &nodeArr[r];
+            } else {
+                nodeArr[idx].num = stoi(op);
+            }
+        }
+        cout << postOrder(nodeArr[1]) << "\n";
+    }
+
+    return 0;
+}
