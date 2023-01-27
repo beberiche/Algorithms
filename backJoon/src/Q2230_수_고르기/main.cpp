95c8ef97bf7e0c013467572a77180d6c8d34bfe7
beberiche <woaol@naver.com>
2023-01-27 10:23:40 +0900

    230127 BOJ

---
+++ backJoon/src/Q2230_수_고르기/main.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#define MAX 2000000001
+using namespace std;
+int N, M, st, ed;
+int arr[100000];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+    sort(arr, arr + N);
+    int res = MAX;
+    while (st < N && ed < N && st <= ed) {
+        int diff = arr[ed] - arr[st];
+        if (diff < M)
+            ed++;
+        else {
+            res = min(res, diff);
+            st++;
+        }
+    }
+    cout << res;
+}
