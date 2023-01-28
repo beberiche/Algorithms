f3e06cc0bebe29d027fa66e0e950cc615618c003
beberiche <woaol@naver.com>
2023-01-28 21:55:40 +0900

    230128 SWEA

--- SWEA/src/Q13072_병사관리_unsolved/Solution.java
+++ SWEA/src/Q13072_병사관리_unsolved/Solution.java
@@ -1,103 +1,103 @@
-package Q13072_병사관리_unsolved;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-class Solution
-{
-    private final static int CMD_INIT				= 1;
-    private final static int CMD_HIRE				= 2;
-    private final static int CMD_FIRE				= 3;
-    private final static int CMD_UPDATE_SOLDIER		= 4;
-    private final static int CMD_UPDATE_TEAM		= 5;
-    private final static int CMD_BEST_SOLDIER		= 6;
-
-    private final static UserSolution usersolution = new UserSolution();
-
-    private static boolean run(BufferedReader br) throws Exception
-    {
-        StringTokenizer st;
-
-        int numQuery;
-
-        int mID, mTeam, mScore, mChangeScore;
-
-        int userAns, ans;
-
-        boolean isCorrect = false;
-
-        numQuery = Integer.parseInt(br.readLine());
-
-        for (int q = 0; q < numQuery; ++q)
-        {
-            st = new StringTokenizer(br.readLine(), " ");
-
-            int cmd;
-            cmd = Integer.parseInt(st.nextToken());
-
-            switch(cmd)
-            {
-                case CMD_INIT:
-                    usersolution.init();
-                    isCorrect = true;
-                    break;
-                case CMD_HIRE:
-                    mID = Integer.parseInt(st.nextToken());
-                    mTeam = Integer.parseInt(st.nextToken());
-                    mScore = Integer.parseInt(st.nextToken());
-                    usersolution.hire(mID, mTeam, mScore);
-                    break;
-                case CMD_FIRE:
-                    mID = Integer.parseInt(st.nextToken());
-                    usersolution.fire(mID);
-                    break;
-                case CMD_UPDATE_SOLDIER:
-                    mID = Integer.parseInt(st.nextToken());
-                    mScore = Integer.parseInt(st.nextToken());
-                    usersolution.updateSoldier(mID, mScore);
-                    break;
-                case CMD_UPDATE_TEAM:
-                    mTeam = Integer.parseInt(st.nextToken());
-                    mChangeScore = Integer.parseInt(st.nextToken());
-                    usersolution.updateTeam(mTeam, mChangeScore);
-                    break;
-                case CMD_BEST_SOLDIER:
-                    mTeam = Integer.parseInt(st.nextToken());
-                    userAns = usersolution.bestSoldier(mTeam);
-                    ans = Integer.parseInt(st.nextToken());
-                    System.out.println("현재 best soilder : " + userAns);
-                    if (userAns != ans) {
-                        isCorrect = false;
-                    }
-                    break;
-                default:
-                    isCorrect = false;
-                    break;
-            }
-        }
-
-        return isCorrect;
-    }
-
-    public static void main(String[] args) throws Exception
-    {
-        int TC, MARK;
-
-        //System.setIn(new java.io.FileInputStream("res/sample_input.txt"));
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        TC = Integer.parseInt(st.nextToken());
-        MARK = Integer.parseInt(st.nextToken());
-
-        for (int testcase = 1; testcase <= TC; ++testcase)
-        {
-            int score = run(br) ? MARK : 0;
-            System.out.println("#" + testcase + " " + score);
-        }
-
-        br.close();
-    }
-}
+//package Q13072_병사관리_unsolved;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//class Solution
+//{
+//    private final static int CMD_INIT				= 1;
+//    private final static int CMD_HIRE				= 2;
+//    private final static int CMD_FIRE				= 3;
+//    private final static int CMD_UPDATE_SOLDIER		= 4;
+//    private final static int CMD_UPDATE_TEAM		= 5;
+//    private final static int CMD_BEST_SOLDIER		= 6;
+//
+//    private final static UserSolution usersolution = new UserSolution();
+//
+//    private static boolean run(BufferedReader br) throws Exception
+//    {
+//        StringTokenizer st;
+//
+//        int numQuery;
+//
+//        int mID, mTeam, mScore, mChangeScore;
+//
+//        int userAns, ans;
+//
+//        boolean isCorrect = false;
+//
+//        numQuery = Integer.parseInt(br.readLine());
+//
+//        for (int q = 0; q < numQuery; ++q)
+//        {
+//            st = new StringTokenizer(br.readLine(), " ");
+//
+//            int cmd;
+//            cmd = Integer.parseInt(st.nextToken());
+//
+//            switch(cmd)
+//            {
+//                case CMD_INIT:
+//                    usersolution.init();
+//                    isCorrect = true;
+//                    break;
+//                case CMD_HIRE:
+//                    mID = Integer.parseInt(st.nextToken());
+//                    mTeam = Integer.parseInt(st.nextToken());
+//                    mScore = Integer.parseInt(st.nextToken());
+//                    usersolution.hire(mID, mTeam, mScore);
+//                    break;
+//                case CMD_FIRE:
+//                    mID = Integer.parseInt(st.nextToken());
+//                    usersolution.fire(mID);
+//                    break;
+//                case CMD_UPDATE_SOLDIER:
+//                    mID = Integer.parseInt(st.nextToken());
+//                    mScore = Integer.parseInt(st.nextToken());
+//                    usersolution.updateSoldier(mID, mScore);
+//                    break;
+//                case CMD_UPDATE_TEAM:
+//                    mTeam = Integer.parseInt(st.nextToken());
+//                    mChangeScore = Integer.parseInt(st.nextToken());
+//                    usersolution.updateTeam(mTeam, mChangeScore);
+//                    break;
+//                case CMD_BEST_SOLDIER:
+//                    mTeam = Integer.parseInt(st.nextToken());
+//                    userAns = usersolution.bestSoldier(mTeam);
+//                    ans = Integer.parseInt(st.nextToken());
+//                    System.out.println("현재 best soilder : " + userAns);
+//                    if (userAns != ans) {
+//                        isCorrect = false;
+//                    }
+//                    break;
+//                default:
+//                    isCorrect = false;
+//                    break;
+//            }
+//        }
+//
+//        return isCorrect;
+//    }
+//
+//    public static void main(String[] args) throws Exception
+//    {
+//        int TC, MARK;
+//
+//        //System.setIn(new java.io.FileInputStream("res/sample_input.txt"));
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//        TC = Integer.parseInt(st.nextToken());
+//        MARK = Integer.parseInt(st.nextToken());
+//
+//        for (int testcase = 1; testcase <= TC; ++testcase)
+//        {
+//            int score = run(br) ? MARK : 0;
+//            System.out.println("#" + testcase + " " + score);
+//        }
+//
+//        br.close();
+//    }
+//}
