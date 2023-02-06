3ec089cb320767ebecc60e039cc2c3280f22bfe6
beberiche <woaol@naver.com>
2023-02-06 15:03:39 +0900

    230206 BOJ 트리와 쿼리

--- backJoon/src/Q15681_트리와_쿼리/Main.java
+++ backJoon/src/Q15681_트리와_쿼리/Main.java
@@ -1,59 +1,83 @@
 package Q15681_트리와_쿼리;
 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-
-class Tree {
-    int node;
-    List<Integer> child;
-
-    public Tree(int node) {
-        this.node = node;
-        this.child = new ArrayList<>();
-    }
-
-}
+import java.util.StringTokenizer;
 
 public class Main {
-    static Tree[] map;
-    static int ans;
-    static boolean[] visited;
+    static class Tree {
+        int num;
+        List<Tree> child = new ArrayList<>();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int R = sc.nextInt();
-        int Q = sc.nextInt();
-        map = new Tree[N + 1];
-
-        for (int i = 1; i <= N; i++)
-            map[i] = new Tree(i);
-
-        for (int i = 1; i <= N - 1; i++) {
-            int p = sc.nextInt();
-            int c = sc.nextInt();
-            map[p].child.add(c);
-            map[c].child.add(p);
+        Tree(int num) {
+            this.num = num;
         }
 
-        StringBuffer sb = new StringBuffer();
-        while (--Q >= 0) {
-            ans = 0;
-            visited = new boolean[N + 1];
-            countChild(map[R], sc.nextInt(), false);
-            sb.append(ans +" \n");
+        void push(Tree tree) {
+            child.add(tree);
         }
-        System.out.println(sb.toString());
     }
 
-    public static void countChild(Tree tree, int currNode, boolean check) {
-        if (visited[tree.node]) return;
-        visited[tree.node] = true;
-        if (tree.node == currNode || check) ans++;
-        for (int c : tree.child) {
-            if (tree.node == currNode || check) countChild(map[c], currNode, true);
-            else countChild(map[c], currNode, false);
+    static int N, R, Q;
+    static Tree[] arr;
+    static int[] childCnt,exp;
+    static boolean[] visited;
+
+    private static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk;
+        stk = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(stk.nextToken());
+        R = Integer.parseInt(stk.nextToken());
+        Q = Integer.parseInt(stk.nextToken());
+
+        arr = new Tree[N + 1];
+        childCnt = new int[N + 1];
+        visited = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = new Tree(i);
         }
+
+
+        for (int i = 0; i < N - 1; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(stk.nextToken());
+            int n2 = Integer.parseInt(stk.nextToken());
+            arr[n1].push(arr[n2]);
+            arr[n2].push(arr[n1]);
+        }
+
+        exp = new int[Q]; // example
+        for (int i = 0; i < Q; i++) {
+            exp[i] = Integer.parseInt(br.readLine());
+        }
+    }
+
+    private static int findChildCnt(Tree curr_tree) {
+        visited[curr_tree.num] = true;
+
+        int num = 1;
+        for (Tree t : curr_tree.child) {
+            if (visited[t.num]) continue;
+            num += findChildCnt(t);
+        }
+        childCnt[curr_tree.num] = num;
+        return num;
+
+    }
+
+    private static void output(int q) {
+        for (int i = 0; i < q; i++) {
+            System.out.println(childCnt[exp[i]]);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+        findChildCnt(arr[R]);
+        output(Q);
     }
 }
