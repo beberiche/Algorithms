c81acd124553b9b5152c3dc2bd98023f10ee1404
beberiche <woaol@naver.com>
2023-02-18 23:46:44 +0900

    230218 알고이론 KMP

---
+++ 알고이론/src/KMP/Main.java
@@ -0,0 +1,51 @@
+package KMP;
+
+public class Main {
+    static String parent, pattern;
+    static int table[];
+    public static void main(String[] args) {
+        parent = "ABACAABCCAABACAABA";
+        pattern = "ABC";
+
+        makeTable(parent);
+
+        search(parent, pattern);
+    }
+
+    private static void search(String str, String pattern) {
+        int sLen = str.length();
+        int pLen = pattern.length();
+
+        int j=0;
+        for(int i=0; i<sLen; i++) {
+            while(j>0 && str.charAt(i) != pattern.charAt(j)) {
+                j = table[j-1];;
+            }
+
+            if(str.charAt(i) == pattern.charAt(j)) {
+                if(j == pLen-1) {
+                    System.out.println(i-pLen+1+" "+ (i));
+                    j = table[j];
+                } else {
+                    j++;
+                }
+            }
+        }
+    }
+
+    private static void makeTable(String pattern) {
+        int pLen = pattern.length();
+        table = new int[pLen];
+
+        int j=0;
+        for(int i=1; i<pLen; i++) {
+            while(j>0 && pattern.charAt(i) != pattern.charAt(j)) {
+                j = table[j-1];
+            }
+
+            if(pattern.charAt(i)==pattern.charAt(j)) {
+                table[i] = ++j;
+            }
+        }
+    }
+}
