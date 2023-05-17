e901a62564a5cbe8080989e22d35e71cbec7677e
beberiche <woaol@naver.com>
2023-05-17 23:53:17 +0900

    230517 BOJ 동전뒤집기

--- backJoon/src/Q1285_동전_뒤집기/main.cpp
+++ backJoon/src/Q1285_동전_뒤집기/main.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int N, a[21], ans = 401;
+int N, a[24], ans = 401;
 void input() {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
     cin >> N;
     string s;
-    for (int i = 1; i <= N; i++) {
+    for (int i = 0; i < N; i++) {
         cin >> s;
-        for (int j = 0; j < s.size(); j++) {
+        for (int j = 0; j < N; j++) {
             if (s[j] == 'T') a[i] += (1 << j);
         }
     }
 }
 
 void solve(int d) {
-    if (d > N) {
+    if (d >= N) {
         int t = 0;
         for (int i = 1; i <= (1 << (N - 1)); i *= 2) {
             int cnt = 0;
-            for (int j = 1; j <= N; j++) if (a[j] & i) cnt++;
+            for (int j = 0; j < N; j++) if (a[j] & i) cnt++;
             t += min(cnt, N - cnt);
         }
         ans = min(ans, t);
@@ -38,6 +38,6 @@ void output() {
 
 int main() {
     input();
-    solve(1);
+    solve(0);
     output();
 }
