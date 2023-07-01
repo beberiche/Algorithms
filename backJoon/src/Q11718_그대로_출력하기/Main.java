8323618cfe805357171a90d0fb2af54e972e082e
beberiche <woaol@naver.com>
2023-07-01 16:42:21 +0900

    230701 BOJ 그대로 출력

---
+++ backJoon/src/Q11718_그대로_출력하기/Main.java
@@ -0,0 +1,14 @@
+package Q11718_그대로_출력하기;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while((s=br.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+}
