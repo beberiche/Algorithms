beccae1c26e808c1b3cd43bdc8f9c1788dacf695
beberiche <woaol@naver.com>
2023-09-06 21:50:13 +0900

    230906 BOJ AB2

---
+++ backJoon/src/Q2558_AB2/Main.java
@@ -0,0 +1,10 @@
+package Q2558_AB2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
