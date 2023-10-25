ba2b5e12ffe8c4d6c8b96f2789d9e3efa6e97c32
beberiche <woaol@naver.com>
2023-10-25 22:47:18 +0900

    231025 BOJ 가장 긴 감소하는 부분수열

--- backJoon/src/Q11722_가장_긴_감소하는_부분수열/Main.java
+++ backJoon/src/Q11722_가장_긴_감소하는_부분수열/Main.java
@@ -1,28 +1,33 @@
 package Q11722_가장_긴_감소하는_부분수열;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int[] arr = new int[N];
-		int[] DP = new int[N];
-		int ans = 0;
-		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
-		}
+        int N = Integer.parseInt(br.readLine());
 
-		for (int i = 1; i < N; i++) {
-			DP[i] = 1;
-			for (int j = 0; j < i; j++) {
-				if (arr[j] > arr[i] && DP[j] + 1 > DP[i]) {
-					DP[i] = DP[j] + 1;
-				}
-			}
-			ans = Math.max(DP[i], ans);
-		}
-		System.out.println(ans);
-	}
+        int[] arr = new int[N];
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        int[] dp = new int[N];
+        int ans = 1;
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(dp[i], ans);
+        }
+
+        System.out.println(ans);
+    }
 }
