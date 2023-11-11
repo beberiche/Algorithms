0fb1d8f4426219e553636f67ec272a1626d68291
beberiche <woaol@naver.com>
2023-11-11 23:24:06 +0900

    231111 BOJ 오르막 수

--- backJoon/src/Q11057_오르막_수/Main.java
+++ backJoon/src/Q11057_오르막_수/Main.java
@@ -1,16 +1,17 @@
 package Q11057_오르막_수;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         int sum[] = new int[N + 1];
         int dp[][] = new int[N + 1][10];
         int DIV = 10007;
 
-        for (int i = 1; i <= 9; i++) {
+        for (int i = 1; i < 10; i++) {
             dp[1][i] = 1;
         }
 
@@ -19,11 +20,9 @@ public class Main {
         int ans = sum[1];
         for (int i = 2; i <= N; i++) {
             dp[i][1] = sum[i - 1];
-            sum[i] = sum[i - 1] % DIV;
+            sum[i] = (sum[i] + dp[i][1]) % DIV;
             for (int j = 2; j < 10; j++) {
-                for (int k = j; k < 10; k++) {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][k]) % DIV;
-                }
+                dp[i][j] = (dp[i][j - 1] + DIV - dp[i - 1][j - 1]) % DIV;
                 sum[i] = (sum[i] + dp[i][j]) % DIV;
             }
             ans = (ans + sum[i]) % DIV;
