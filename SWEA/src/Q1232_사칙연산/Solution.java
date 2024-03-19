6cc15d34b7bf6127f6af65afaf7f803da7f47ea7
beberiche <woaol@naver.com>
2024-03-19 19:08:34 +0900

    240319 SWEA 사칙연산

--- SWEA/src/Q1232_사칙연산/Solution.java
+++ SWEA/src/Q1232_사칙연산/Solution.java
@@ -1,9 +1,7 @@
 package Q1232_사칙연산;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) throws Exception {
@@ -49,18 +47,18 @@ public class Solution {
 					double data = node[idx][0];
 
 					switch (oper) {
-					case "+":
-						data = ln + rn;
-						break;
-					case "-":
-						data = ln - rn;
-						break;
-					case "*":
-						data = ln * rn;
-						break;
-					case "/":
-						data = ln / rn;
-						break;
+						case "+":
+							data = ln + rn;
+							break;
+						case "-":
+							data = ln - rn;
+							break;
+						case "*":
+							data = ln * rn;
+							break;
+						case "/":
+							data = ln / rn;
+							break;
 
 					}
 
