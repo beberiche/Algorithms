4aa12eba8e04153adeca0a7598a4f0d38a10c13f
JongHyeon Kim (Bell) <woaol@naver.com>
2024-04-16 11:43:09 +0900

    240416 BOJ 연세대학교

---
+++ backJoon/src/Q15680_연세대학교/Main.java
@@ -0,0 +1,9 @@
+package Q15680_연세대학교;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(new Scanner(System.in).nextInt() == 0 ? "YONSEI": "Leading the Way to the Future");
+	}
+}
