0e40583e05ac284c4ca5e9906ffc6d0c4a8f6871
JongHyeon Kim (Bell) <woaol@naver.com>
2024-07-08 12:30:36 +0900

    240708 BOJ 15665 N과 M 11

--- backJoon/src/Q15665_N과_M_11/Main.java
+++ backJoon/src/Q15665_N과_M_11/Main.java
@@ -2,9 +2,7 @@ package Q15665_N과_M_11;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
