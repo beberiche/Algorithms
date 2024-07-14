5fa0dd7734271b6ddd9f44cf239b9266bd6f14d1
JongHyeon Kim (Bell) <woaol@naver.com>
2024-07-14 21:52:07 +0900

    240714 BOJ 9656 돌 게임2

---
+++ backJoon/src/Q9656_돌_게임_2/Main.java
@@ -0,0 +1,9 @@
+package Q9656_돌_게임_2;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(new Scanner(System.in).nextInt() % 2 == 0 ? "SK" : "CY");
+	}
+}
