5da6090c586626ae703d9874506d5216e28794a1
beberiche <woaol@naver.com>
2024-10-08 00:03:36 +0900

    241008 BOJ 2491 수열

--- backJoon/src/Q2491_수열/Main.java
+++ backJoon/src/Q2491_수열/Main.java
@@ -1,25 +1,36 @@
 package Q2491_수열;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		String str = sc.nextLine();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] a = new int[N];
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int cnt1 = 1;
+        int cnt2 = 1;
+        int ret = 1;
+        a[0] = Integer.parseInt(stk.nextToken());
+        for (int i = 1; i < N; i++) {
+            a[i] = Integer.parseInt(stk.nextToken());
+            if (a[i - 1] <= a[i]) {
+                cnt1++;
+            } else {
+                cnt1 = 1;
+            }
 
-		int max = 0;
-		int cnt1 = 0;
-		int cnt2 = 0;
-		int check = 1;
-		for (int i = 0; i < N - 1; i++) {
-			if (check == 1) {
-				if (str.charAt(i) >= str.charAt(i + 1)) {
-					cnt2++;
-				}
-			} else {
+            if (a[i - 1] >= a[i]) {
+                cnt2++;
+            } else {
+                cnt2 = 1;
+            }
 
-			}
-		}
-	}
+            ret = Math.max(ret, Math.max(cnt1, cnt2));
+        }
+
+        System.out.println(ret);
+    }
 }
