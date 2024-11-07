8e3c303049c3211e36e744c707bc21e182e2f777
beberiche <woaol@naver.com>
2024-11-08 00:52:05 +0900

    241108 BOJ 1233 주사위

--- backJoon/src/Q1233_주사위/Main.java
+++ backJoon/src/Q1233_주사위/Main.java
@@ -18,6 +18,7 @@ public class Main {
             a[i][0] = i;
         }
 
+
         for (int i = 1; i <= s1; i++) {
             for (int j = 1; j <= s2; j++) {
                 for (int k = 1; k <= s3; k++) {
