a7002d75c6a02b2f9124039092cb07cb803db0d5
beberiche <woaol@naver.com>
2024-12-14 00:56:44 +0900

    241213 BOJ 1550 16진수

---
+++ backJoon/src/Q1550_16진수/Main.java
@@ -0,0 +1,10 @@
+package Q1550_16진수;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        System.out.println(Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine(), 16));
+    }
+}
