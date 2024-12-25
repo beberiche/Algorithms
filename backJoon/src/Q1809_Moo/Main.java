88d33ed03f80258850d69009bb12ce82c4066d93
beberiche <woaol@naver.com>
2024-12-25 23:38:41 +0900

    241225 BOJ 1809 Moo

---
+++ backJoon/src/Q1809_Moo/Main.java
@@ -0,0 +1,14 @@
+package Q1809_Moo;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("(___)").append("\n");
+        sb.append("(o o)____/").append("\n");
+        sb.append(" @@      \\").append("\n");
+        sb.append("  \\ ____,/").append("\n");
+        sb.append("  //   //").append("\n");
+        sb.append(" ^^   ^^").append("\n");
+        System.out.print(sb.toString());
+    }
+}
