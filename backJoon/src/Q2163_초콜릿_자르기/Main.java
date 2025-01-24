c3ddcc346be056ab438e640bf83035e8938eab0f
beberiche <woaol@naver.com>
2025-01-25 01:03:40 +0900

    250125 BOJ 2154 초콜릿 자르기

---
+++ backJoon/src/Q2163_초콜릿_자르기/Main.java
@@ -0,0 +1,16 @@
+package Q2163_초콜릿_자르기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        System.out.print(n*m-1);
+    }
+}
