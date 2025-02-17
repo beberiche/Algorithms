276eabd4989a1012810a30b7a72a5839cc894d8c
beberiche <woaol@naver.com>
2025-02-17 21:28:09 +0900

    250217 BOJ 2495 약수 구하기

---
+++ backJoon/src/Q2501_약수_구하기/Main.java
@@ -0,0 +1,29 @@
+package Q2501_약수_구하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+        int result = 0;
+
+        for(int i = 1; i <= N; i++) {
+            if(N % i == 0) {
+                count++;
+            }
+            if(count == K) {
+                result = i;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
