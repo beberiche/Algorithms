22b523322d89f708c2b07ed0086ee083280d51a0
beberiche <woaol@naver.com>
2025-02-25 23:34:22 +0900

    250225 BOJ 2393 Rock

---
+++ backJoon/src/Q2393_Rock/Main.java
@@ -0,0 +1,28 @@
+package Q2393_Rock;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder str = new StringBuilder();
+
+        str.append("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+
+        System.out.print(str);
+        br.close();
+    }
+}
