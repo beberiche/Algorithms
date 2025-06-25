95d9d232f91c4ac1e2be0a943155f404c353a9bf
beberiche <woaol@naver.com>
2025-06-25 22:53:41 +0900

    250625 BOJ 4999 아

---
+++ backJoon/src/Q4999_아/Main.java
@@ -0,0 +1,11 @@
+package Q4999_아;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        System.out.println(br.readLine().length() >= br.readLine().length() ? "go" : "no");
+    }
+}
