33314c2021ad39a749605223ba50b8956e318572
beberiche <woaol@naver.com>
2025-08-24 20:57:30 +0900

    250824 BOJ 15439 베라의 패션

---
+++ backJoon/src/Q15439_베라의_패션/Main.java
@@ -0,0 +1,12 @@
+package Q15439_베라의_패션;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.print(N * (N - 1));
+    }
+}
