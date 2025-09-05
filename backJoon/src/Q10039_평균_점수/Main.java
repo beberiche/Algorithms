5c01662a1b3ba1939612b7a6178a497fc5baada5
beberiche <woaol@naver.com>
2025-09-06 02:36:57 +0900

    250906 BOJ 10039 평균 점수

---
+++ backJoon/src/Q10039_평균_점수/Main.java
@@ -0,0 +1,18 @@
+package Q10039_평균_점수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        for(int i=0; i<5; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num < 40) num = 40;
+            sum += num;
+        }
+        System.out.println(sum / 5);
+    }
+}
