992f841af9a349e78048891ee0f70a5645dee448
beberiche <woaol@naver.com>
2025-09-07 21:45:18 +0900

    250907 BOJ 5893 17배

---
+++ backJoon/src/Q5893_17배/Main.java
@@ -0,0 +1,32 @@
+package Q5893_17배;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger N = new BigInteger(br.readLine(), 2);
+        Stack<BigInteger> stack = new Stack<>();
+
+        N = N.multiply(BigInteger.valueOf(17));
+
+        while(true) {
+            stack.push(N.remainder(BigInteger.valueOf(2)));
+            N = N.divide(BigInteger.valueOf(2));
+            if(N.equals(BigInteger.ONE)) {
+                stack.push(N);
+                break;
+            }
+        }
+
+        StringBuilder sb= new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        System.out.print(sb);
+    }
+}
