a817c072f3bea1f971611005eaa816f851c45543
gimjonghyeon <woaol@naver.com>
2025-09-14 12:28:04 +0900

    250914 BOJ 18310 안테나

---
+++ backJoon/src/Q18310_안테나/Main.java
@@ -0,0 +1,22 @@
+package Q18310_안테나;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] a = new int[N];
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            a[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        Arrays.sort(a);
+        System.out.print(N % 2 == 1 ? a[N / 2] : a[N / 2 - 1]);
+    }
+}
