7f1ef563d01311feb80dae1689ca171587f67140
gimjonghyeon <woaol@naver.com>
2025-09-27 00:52:42 +0900

    250927 BOJ 5524 입실 관리

---
+++ backJoon/src/Q5524_입실_관리/Main.java
@@ -0,0 +1,19 @@
+package Q5524_입실_관리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            sb.append(str.toLowerCase()).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
