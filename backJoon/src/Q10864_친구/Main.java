3f62ca930633b9c32a86df6ddf34af1e75011e14
gimjonghyeon <woaol@naver.com>
2025-11-26 22:56:34 +0900

    251126 BOJ 10864 친구

---
+++ backJoon/src/Q10864_친구/Main.java
@@ -0,0 +1,26 @@
+package Q10864_친구;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] cnt = new int[n + 1];
+        while (m-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            cnt[Integer.parseInt(st.nextToken())]++;
+            cnt[Integer.parseInt(st.nextToken())]++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(cnt[i]).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
