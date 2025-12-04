09878035e2d2a47392706e2106618d4e4d0cc374
gimjonghyeon <woaol@naver.com>
2025-12-05 02:34:35 +0900

    251205 BOJ 10833 사과

---
+++ backJoon/src/Q10833_사과/Main.java
@@ -0,0 +1,22 @@
+package Q10833_사과;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sum += (B % A);
+        }
+        System.out.print(sum);
+    }
+}
