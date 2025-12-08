b8a5a3c1176f65fff804c71fca07f539c73b85e9
gimjonghyeon <woaol@naver.com>
2025-12-08 23:29:39 +0900

    251208 BOJ 5338 마이크로소프트 로고

---
+++ backJoon/src/Q5338_마이크로소프트_로고/Main.java
@@ -0,0 +1,11 @@
+package Q5338_마이크로소프트_로고;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
